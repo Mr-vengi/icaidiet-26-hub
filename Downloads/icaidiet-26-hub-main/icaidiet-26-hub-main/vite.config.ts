9dec7d202b5fdd2de74de32846c0c320bfeabad3
mr_vengi <bplusdesignservices@gmail.com>
2026-09-08 17:21:35 +0530

    Update project

--- Downloads/icaidiet-26-hub-main/icaidiet-26-hub-main/vite.config.ts
+++ Downloads/icaidiet-26-hub-main/icaidiet-26-hub-main/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tanstackStart(), react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    cssMinify: false,
+  },
   server: {
     allowedHosts: ['icaidiet-26-hub.onrender.com'],
   },
